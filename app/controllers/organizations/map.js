5ef03bb01f27f187d28b97905f11be394a6ea392
Michael Martz <blktiger@gmail.com>
2015-04-30 20:35:27 -0500

    Attempt to refactor the map code

--- app/controllers/organizations/map.js
+++ app/controllers/organizations/map.js
@@ -1,40 +1,27 @@
 import Ember from 'ember';
 
-export default Ember.ArrayController.extend({
-    zoom: 8,
-    centerLat: -30.14068,
-    centerLng: 30.136454,
-    allmarkers: [],
-    
-    initMap: function() {
-		console.log("didInsertElement map");
-	}.on('didInsertElement'),
+export default Ember.Controller.extend({
+	zoom: 8,
+	centerLat: -30.14068,
+	centerLng: 30.136454,
 
-    _markers: function() {
-        var self = this;
-        this.get('model').forEach(function(org, i) {
-            //limit results
-            if(i > 50){
-                return;
-            }
-            org.get('profile').then(function(profile) {
-                profile.get('address').then(function(address) {
-                    var lat = address.get('lat');
-                    var lng = address.get('lng');
-                    if (lat && lng) {
-                        var marker = {
-                            id: org.get('id'),
-                            title: profile.get('orgName'),
-                            lat: lat,
-                            lng: lng,
-                            isDraggable: false
-                        }
-                        var markers = self.get('allmarkers').toArray();
-                        markers.push(marker);
-                        self.set('allmarkers', Ember.A(markers));
-                    }
-                });
-            });
-        });
-    }.observes('model.@each.org.profile.address')
+	markers: function() {
+		var markers = this.get('model').filter(function(org, index) {
+			return index <= 50;
+		}).map(function(org) {
+			return {
+				id: org.get('id'),
+				title: org.get('profile.orgName'),
+				lat: org.get('profile.address.lat'),
+				lng: org.get('profile.address.lng'),
+				isDraggable: false
+			};
+		});
+
+		return markers;
+	}.observes('model.@each.org.profile.address'),
+
+	init: function() {
+		window.map = this;
+	}
 });
