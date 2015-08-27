53b3436aea4cf199c4971665ba2cf29192aabf66
Martz, Michael <Michael.Martz@garmin.com>
2015-08-27 14:07:26 -0500

    Convert template to components

--- app/components/forms/edit-question.js
+++ app/components/forms/edit-question.js
@@ -1,9 +1,19 @@
 import Ember from "ember";
 
 export default Ember.Component.extend({
-	questionComponent: Ember.computed("type", {
+	questionComponent: Ember.computed('selectedType', {
 		get: function() {
-			return `forms/edit-${this.get('type')}-question`;
+			return `forms/edit-${this.get('selectedType')}-question`;
 		}
-	})
+	}),
+
+	selectedType: 'short-answer',
+
+	types: [
+		{label: 'Short Answer', value: 'short-answer'},
+		{label: 'Long Answer', value: 'long-answer'},
+		{label: 'Checkbox', value: 'checkbox'},
+		{label: 'Single Choice', value: 'single-choice'},
+		{label: 'Multiple Choice', value: 'multiple-choice'}
+	]
 });
